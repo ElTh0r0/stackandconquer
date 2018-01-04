b43e6f17d55cbf36c64a35b70be69559361ca9d4
ElTh0r0 <elthoro@gmx.de>
2018-01-04 17:55:49 +0100

    macOS build script

---
+++ macOS/build_macOS.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+# Hold on to current directory
+project_dir=$(pwd)
+
+# Output macOS version
+sw_vers
+
+# Update platform
+echo "Updating platform..."
+brew update
+
+# Install p7zip for packaging
+brew install p7zip
+
+# Install Qt
+echo "Installing Qt..."
+brew install qt
+
+# Add Qt binaries to path
+PATH=/usr/local/opt/qt/bin/:${PATH}
+
+# Build app
+echo "Building..."
+cd ${project_dir}
+qmake -config release
+make
+
+# Build and run tests here
+
+# Package
+echo "Packaging..."
+cd ${project_dir}/build/macOS/clang/x86_64/release/
+
+# Remove build directories that should not be deployed
+rm -rf moc
+rm -rf obj
+rm -rf qrc
+
+echo "Creating dmg archive..."
+macdeployqt iQPuzzle.app -qmldir=../../../../../src -dmg
+mv iQPuzzle.dmg "iQPuzzle_${TAG_NAME}.dmg"
+
+# Copy other project files
+cp "${project_dir}/README.md" "README.md"
+cp "${project_dir}/COPYING" "COPYING"
+cp -r "${project_dir}/data/cpu/" "cpu/"
+
+echo "Packaging zip archive..."
+7z a iQPuzzle_${TAG_NAME}_macos.zip "iQPuzzle_${TAG_NAME}.dmg" "README.md" "COPYING" "cpu/"
+
+echo "Done!"
+
+exit 0
