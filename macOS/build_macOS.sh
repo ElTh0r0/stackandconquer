863c5d7cb2332cf9bab870f82b1349c310e7db7a
ElTh0r0 <elthoro@gmx.de>
2018-01-04 23:59:57 +0100

    macOS build script

--- macOS/build_macOS.sh
+++ macOS/build_macOS.sh
@@ -25,14 +25,15 @@ PATH=/usr/local/opt/qt/bin/:${PATH}
 # Build app
 echo "Building..."
 cd ${project_dir}
-qmake -config release
+mkdir build
+cd build
+qmake ../stackandconquer.pro CONFIG+=release
 make
-ls -lRa .
+
 # Build and run tests here
 
 # Package
 echo "Packaging..."
-cd ${project_dir}/build/macOS/clang/x86_64/release/
 
 # Remove build directories that should not be deployed
 rm -rf moc
@@ -44,9 +45,9 @@ macdeployqt StackAndConquer.app -dmg
 mv StackAndConquer.dmg "StackAndConquer_${TAG_NAME}.dmg"
 
 # Copy other project files
-cp "${project_dir}/README.md" "README.md"
-cp "${project_dir}/COPYING" "COPYING"
-cp -r "${project_dir}/data/cpu/" "cpu/"
+cp "${project_dir}/README.md" "${project_dir}/build/README.md"
+cp "${project_dir}/COPYING" "${project_dir}/build/COPYING"
+cp -r "${project_dir}/data/cpu/" "${project_dir}/build/cpu/"
 
 echo "Packaging zip archive..."
 7z a StackAndConquer_${TAG_NAME}_macos.zip "StackAndConquer_${TAG_NAME}.dmg" "README.md" "COPYING" "cpu/"
