19ad4989f670bb151f3270ef268dba34381ecd7d
ElTh0r0 <elthoro@gmx.de>
2018-01-05 17:59:53 +0100

    build_AppImage

--- AppImage/build_AppImage.sh
+++ AppImage/build_AppImage.sh
@@ -13,8 +13,7 @@ unset QTDIR ; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=$(git rev-parse --short HEAD)  # linuxdeployqt uses this for naming the file
 
 echo "Creating AppImage..."
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
-./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -appimage
+./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -appimage -bundle-non-qt-libs
 find ./appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 
 echo "Uploading..."
